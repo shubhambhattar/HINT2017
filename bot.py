4e1d2cc88afdb23f84d35a822a4392f7b6bf1c3a
Shubham Bhattar <shubhambhattar@gmail.com>
2017-03-25 11:12:03 +0530

    Added comments. Query address for Google Maps now checks if some attribute is Not Available (NA).

--- bot.py
+++ bot.py
@@ -1,17 +1,47 @@
 import sys, csv, time, json
 import telepot
 
+# store the data from CSV file to this variable as a list of tuples
 blood_data = []
 
+def get_address(bloodbank):
+    name, address, city = bloodbank['Name'], bloodbank['Address'], bloodbank['City']
+    found_name, found_address = False, False
+
+    query_address = ''
+    if name != 'NA':
+        found_name = True
+        query_address += name
+    if address != 'NA':
+        if found_name:
+            query_address += ', ' + address
+        else:
+            query_address += address
+        found_address = True
+
+    if city != 'NA':
+        if found_address:
+            query_address += ', ' + city
+        else:
+            if found_name:
+                query_address += ', ' + city
+            else:
+                query_address += city
+
+    return query_address
+
 def handle(msg):
+
+    # find the basic information from Message object
     content_type, chat_type, chat_id = telepot.glance(msg)
     print(content_type, chat_type, chat_id)
 
-
+    # if the content_type is not text, then we can safely ignore the request
     if content_type != 'text':
         bot.sendMessage(chat_id, "I don't understand.")
         return
 
+    # boolean value to check whether the variable is found or not
     found = False
     bloodbank = {
         'Name': '',
@@ -23,42 +53,55 @@ def handle(msg):
     }
     bot.sendMessage(chat_id, 'Retrieving Blood Banks in ' + msg['text'])
     for i in range(len(blood_data)):
+
+        # check whether the given city is in the database or not
         if blood_data[i][2] == msg['text']:
 
-            temp = ''
+            # indexes of the required attributes from the `data/bloodbanks.csv`
             indexes = [4, 5, 2, 1, 6, 7]
             attributes = ['Name', 'Address', 'City', 'State', 'Pincode', 'Contact']
+            temp = ''
+
             for j in range(6):
                 bloodbank[attributes[j]] = blood_data[i][indexes[j]]
                 if attributes[j] == 'Contact':
                     temp += attributes[j] + ': \n'
+
+                    # split in different lines if there are multiple contact numbers
                     numbers = bloodbank[attributes[j]].split(',')
                     for value in numbers:
-                        temp += '<a href="tel:' + str(value) + '">' + str(value) + '</a>\n'
-                        # temp += '<a href="http://www.github.com/">Github</a>\n'
-                        # temp += '<a href="tel:18888888888">8888888888</a>\n'
+                        temp += '<a href="tel//:' + str(''.join(value.split(' '))) + '/">' + str(value) + '</a>\n'
                 elif attributes[j] == 'Address':
                     temp += attributes[j] + ': ' + bloodbank['Address'] + '\n\n'
-                    add = '%20'.join((bloodbank['Name'] + ' ' + bloodbank['Address'] + ' ' + bloodbank['City']).split(' '))
-                    temp += '<a href="http://maps.google.com/?q=' + str(add) + '">' + 'Find in Google Maps' + '</a>\n\n'
+                    query_address = get_address(bloodbank)
+                    query_address = '%20'.join(query_address.split(' '))
+                    temp += '<a href="http://maps.google.com/?q=' + str(query_address) + '">' + 'Find in Google Maps</a>\n\n'
                 else:
                     temp += attributes[j] + ': ' + bloodbank[attributes[j]] + '\n\n'
 
+            # send the message and set the parse_mode to `HTML` to correctly
+            # format hyperlinks - Google Maps and Contact details
             bot.sendMessage(chat_id, temp, parse_mode='HTML')
             found = True
+
+    # send an appropriate response if not found
     if not found:
         bot.sendMessage(chat_id, 'Sorry, no Blood Banks found.')
 
-TOKEN = sys.argv[1]
-bot = telepot.Bot(TOKEN)
-bot.message_loop(handle)
-print('Listening...')
-
+# extract data from `data/bloodbank.csv` and store it in `blood_data`
 with open('data/bloodbank.csv', 'rb') as csvfile:
     datareader = csv.reader(csvfile)
     for row in datareader:
         blood_data.append(tuple(row))
 
+# delete the first row that contains metadata
 del blood_data[0]
+
+# get TOKEN of Telegram bot from command line
+TOKEN = sys.argv[1]
+bot = telepot.Bot(TOKEN)
+bot.message_loop(handle)
+print('Listening...')
+
 while True:
     time.sleep(10)
